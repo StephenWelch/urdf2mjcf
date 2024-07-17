e8e4f5e7365e841011b55af18049ed5886e1b884
Stephen Welch <7754730+StephenWelch@users.noreply.github.com>
2024-07-16 23:12:06 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     version="2.0.4",
-    scripts=["scripts/urdf2mjcf", "scripts/rd2urdf"],
+    scripts=["scripts/urdf2mjcf"],
     packages=["urdf2mjcf"],
     package_dir={"": "src"},
 )
